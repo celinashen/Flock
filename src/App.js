3f2c3084479114acb24c72eb5d70c2d9e81be840
Scott <scottyunho239@gmail.com>
2021-04-30 21:00:23 -0400

    Added login functionality.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import Typography from '@material-ui/core/Typography';
 import TitleIntro from './TitleIntro';
 import FlockList from './FlocksList';
 
+import withFirebaseAuth from 'react-with-firebase-auth'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
 // const THEME = createMuiTheme({
 //   typography: {
 //     fontFamily: [
@@ -17,7 +25,7 @@ import FlockList from './FlocksList';
 
 
 
-const App=()=> {
+const App = ({ user, signOut, signInWithGoogle }) => {
   return (
     <ThemeProvider>
         <div className="App">
@@ -25,8 +33,33 @@ const App=()=> {
           <TitleIntro/>
           <FlockList/>
         </div>
+        
+        <div className="App">
+          <header className="App-header">
+            {
+              user 
+                ? <p>Hello, {user.displayName}</p>
+                : <p>Please sign in.</p>
+            }
+            {
+              user
+                ? <button onClick={signOut}>Sign out</button>
+                : <button onClick={signInWithGoogle}>Sign in with Google</button>
+            }
+          </header>
+        </div>
     </ThemeProvider>
   );
 }
 
-export default App;
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
