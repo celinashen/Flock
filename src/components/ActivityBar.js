0433e60a7d2b00d93a2e32e5384cdb4be1c279e8
Celina <celina.shen06@gmail.com>
2021-05-01 19:51:18 -0400

    added side bar with formatted activity cards

--- src/components/ActivityBar.js
+++ src/components/ActivityBar.js
@@ -7,11 +7,13 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+
+import { ActivityBarData } from './ActivityBarData';
 
 import { Link } from 'react-router-dom';
 import * as GiIcons from "react-icons/gi";
 import * as AiIcons from "react-icons/ai";
-
 
 
 const UseStyles = makeStyles((theme) => ({
@@ -20,40 +22,105 @@ const UseStyles = makeStyles((theme) => ({
     "fontSize": 80,
     "color": "#ADD7AD"
   },
+  //activity bar before prompt
   activityBar:{
-    //backgroundColor: "rgba(173, 215, 173,0.95)", 
+    backgroundColor: "rgba(173, 215, 173,0.95)", 
     //maxWidth: 100,
-  }
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'fixed',
+    top: 0,
+    //right: '-100%',
+    transition: '850ms',
+    
+  },
+  //activity bar styling after prompted
+  activityBarActive:{
+    right: 0,
+    transition: '350ms'
+  },
+
+  //each card style
+  activityCardStyle:{
+    backgroundColor: "",
+    marginBottom: 16,
+    paddingLeft: 20,
+    paddingRight: 20, 
+    maxWidth: 275
+  },
+  //transaction in activity card
+  cardTransaction:{
+    "fontFamily": "Poppins",
+    "fontSize": 20,
+    "color": "#ADD7AD",
+    display: "flex"
+  },
+  //Date + Flock name in activity card
+  cardDetails:{
+    "fontFamily": "Poppins",
+    "fontSize": 15,
+    "color": "#ADD7AD",
+  },
+  cardTextInfo:{
+    display: 'flex',
+    justifyContent: 'start',
+    alignItems: 'center',
+  },
+  cardTestTransaction:{
+
+  },
+  activityIcon:{
+    
+  },
+  
  
 }));
 
 const ActivityIcon=()=> {
   const classes = UseStyles();
+  
   const [sidebar, setSidebar] = useState(false)
 
   const showSidebar = () => setSidebar(!sidebar)
+
   return (
     <>
-      <div className = {classes.activityBar}>
+      <div>
         <Grid justify="flex-end" alignItems="flex-end">
-          <Link to="#" className = 'menu-bars'>
+          <Link to="#" className = {classes.activityIcon}>
             <GiIcons.GiGoose onClick={showSidebar} 
               style={{
                   position: 'absolute',
                   right: 40,
-                  bottom: 40
+                  bottom: 40,
               }}
             />
           </Link>
         </Grid>
       </div>
-      <nav className = {sidebar ? 'nav-menu active' : 'nav-menu'}>
+      <nav className = {sidebar ? classes.activityBarActive : classes.activityBar}>
         <ul className = 'nav-menu-items'>
           <li className = "navbar-toggle">
             <Link to = "#" className = 'menu-bars'>
               <AiIcons.AiFillCloseCircle/>
             </Link>
           </li>
+          {ActivityBarData.map((item, index) => {
+            return (
+              
+                <li key={index}>
+                  <Card className = {classes.activityCardStyle}>
+                    <span className = {classes.cardDetails}>{item.date}</span>
+                    <br/>
+                    <span className = {classes.cardDetails}>{item.flock}</span>
+                    <br/>
+                    <span className = {classes.cardTransaction}>{item.transaction}</span>
+                  </Card>
+                </li>
+              
+            )
+          })}
         </ul>
       </nav>
     </>
---
+++ src/components/ActivityBarData.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import * as GiIcons from "react-icons/gi";
+import * as AiIcons from "react-icons/ai";
+
+//paths will be routed to the flock page - should add another attributed called path: '' 
+//this data should be put into firebase and extracted from firebase in the future
+
+
+export const ActivityBarData = [
+    {
+        date: 'march 13, 2020',
+        flock: 'rent',
+        transaction: 'Angela issued you a credit for $130',
+        transactionID: '1'
+    },
+    {
+        date: 'march 13, 2020',
+        flock: 'rent',
+        transaction: 'Angela issued you a credit for $130',
+        transactionID: '2'
+    },
+    {
+        date: 'march 13, 2020',
+        flock: 'rent',
+        transaction: 'Angela issued you a credit for $130',
+        transactionID: '3'
+    }
+]
