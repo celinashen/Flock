ea2a10ef172e74b576ac0dfdb9f0aba0df8b6fef
Celina <celina.shen06@gmail.com>
2021-06-05 20:29:56 -0400

    Added other menu icons - need to work on repositioning

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -9,7 +9,11 @@ import MenuIcon from '@material-ui/icons/Menu';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 import { IconDashboard } from '@tabler/icons';
+import { IconFeather } from '@tabler/icons';
+import { IconCash } from '@tabler/icons';
+import { IconBrandTelegram } from '@tabler/icons';
 import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
 
 //import createMuiTheme from '@material-ui/styles';
 
@@ -25,11 +29,10 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: "Poppins",
     fontSize: 20,
     fontStyle: 'normal',
-    
     fontSize: '15px',
-    lineHeight: '45px',
     textAlign: 'center',
-    letterSpacing: '0.07em'
+    letterSpacing: '0.07em',
+    alignItems: "center"
   },
   homeButton:{
     fontFamily: "Poppins",
@@ -64,10 +67,18 @@ const useStyles = makeStyles((theme) => ({
   },
   menuIcon: {
     backgroundColor: 'white',
-    width: '5vh',
+    maxWidth: '5vh',
+    minWidht: '5vh',
     minHeight: '5vh',
     borderRadius: '10px',
-    boxShadow: 'none'
+    boxShadow: 'none',
+    alignContent: 'center',
+    justifyContent: 'center',
+    display:'flex', 
+  },
+  menuIconNav:{
+    marginTop: '10%',
+    textAlign: 'center'
   }
 }));
 
@@ -94,39 +105,79 @@ function HeaderBar() {
             <Emoji symbol="🦆" className = {classes.loonTheDuck}/>
           </Typography>
 
-          <Link to='/home' className = {classes.linkStyle}> 
+          <Grid 
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            style={{ minHeight: '100vh', marginTop: '20%' }}
+          >
 
-            <Card className = {classes.menuIcon}>
-              <IconDashboard 
-                size={36} // set custom `width` and `height`
-                color="#309F5E" // set `stroke` color
-                stroke={3}  // set `stroke-width`
-                strokeLinejoin="miter" // override other SVG props
-              />
-            </Card>
+            <Grid item xs = {6} alignItems="center" className = {classes.menuIconNav}> 
+              <Link to='/home' className = {classes.linkStyle}> 
+                <Card className = {classes.menuIcon}>
+                  <IconDashboard 
+                    size={36} // set custom `width` and `height`
+                    color="#309F5E" // set `stroke` color
+                    stroke={1.5}  // set `stroke-width`
+                    strokeLinejoin="miter" // override other SVG props
+                  />
+                </Card>
+                <Typography color="inherit" className={classes.typography}>
+                    your dashboard
+                </Typography>
+              </Link>
+            </Grid>      
 
-            <Typography color="inherit" className={classes.typography}>
-              your dashboard
-            </Typography>
-          </Link>
+            <Grid className = {classes.menuIconNav}>
+              <Link to = '/yourflocks' className = {classes.linkStyle}>
+                <Card className = {classes.menuIcon}>
+                  <IconFeather 
+                    size={36} // set custom `width` and `height`
+                    color="#309F5E" // set `stroke` color
+                    stroke={1.5}  // set `stroke-width`
+                    strokeLinejoin="miter" // override other SVG props
+                  />
+                </Card>
+                <Typography color="inherit" className={classes.typography}>
+                  your flocks
+                </Typography>
+              </Link>
+            </Grid>  
 
-          <Link to = '/yourflocks' className = {classes.linkStyle}>
-            <Typography color="inherit" className={classes.typography}>
-              your flocks
-            </Typography>
-          </Link>
+            <Grid item xs={3} className = {classes.menuIconNav}>
+              <Link to = "/issue" className = {classes.linkStyle}>
+                <Card className = {classes.menuIcon}>
+                  <IconCash
+                    size={36} // set custom `width` and `height`
+                    color="#309F5E" // set `stroke` color
+                    stroke={1.5}  // set `stroke-width`
+                    strokeLinejoin="miter" // override other SVG props
+                  />
+                </Card>
+                <Typography color="inherit" className={classes.typography}>
+                  Receivables
+                </Typography>
+              </Link>
+            </Grid>  
 
-          <Link to = "/issue" className = {classes.linkStyle}>
-            <Typography color="inherit" className={classes.typography}>
-              issue debits
-            </Typography>
-          </Link>
+            <Grid item xs={3} className = {classes.menuIconNav}>
+              <Link to ='/pay' className = {classes.linkStyle}>
+                <Card className = {classes.menuIcon}>
+                  <IconBrandTelegram
+                    size={36} // set custom `width` and `height`
+                    color="#309F5E" // set `stroke` color
+                    stroke={1.5}  // set `stroke-width`
+                    strokeLinejoin="miter" // override other SVG props
+                  />
+                </Card>
+                <Typography color="inherit" className={classes.typography}>
+                  Payables
+                </Typography>
+              </Link>
+            </Grid>  
 
-          <Link to ='/pay' className = {classes.linkStyle}>
-            <Typography color="inherit" className={classes.typography}>
-              pay credits
-            </Typography>
-          </Link>
+          </Grid>
       </div>
     </div>
   );
